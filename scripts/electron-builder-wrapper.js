d59fe3c15cc3ae9a018591da6c4d2ce02112cc65
s.gologuzov <s.gologuzov@keysoftware.ru>
2024-03-20 11:22:34 +0300

    Убрана сборка 32-битной архитектуры

--- scripts/electron-builder-wrapper.js
+++ scripts/electron-builder-wrapper.js
@@ -68,7 +68,7 @@ const runBuilder = function (wrapperConfig, target) {
         }
     }
     if (target.platform === 'win32' && wrapperConfig.mode !== 'dev') {
-        allArgs.push('--ia32', '--x64');
+        allArgs.push('--x64');
     }
     if (!wrapperConfig.doPackage) {
         allArgs.push('--dir', '--c.compression=store');
